8b541cc825cccf6cc611f9f5a1a9797859f245c0
Vito <cuicvito@gmail.com>
2024-07-30 23:48:46 +0200

    initial

---
+++ go.mod
@@ -0,0 +1,38 @@
+module main
+
+go 1.22.1
+
+require github.com/mdp/qrterminal v1.0.1
+
+require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pterm/pterm v0.12.79 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/term v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/rs/zerolog v1.33.0 // indirect
+	go.mau.fi/libsignal v0.1.1 // indirect
+	go.mau.fi/util v0.6.0 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20240726213518-bb5852f056ca // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	waLog "go.mau.fi/whatsmeow/util/log"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// Used example code from https://pkg.go.dev/go.mau.fi/whatsmeow
+	dbLog := waLog.Stdout("Database", "ERROR", true)
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:login.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "ERROR", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	params := whatsmeow.GetProfilePictureParams{
+		Preview:     false,
+		ExistingID:  "",
+		IsCommunity: false,
+	}
+
+	user := client.Store.ID.User
+	server := client.Store.ID.Server
+
+	println("Using:", user)
+	jid, err := types.ParseJID(user + "@" + server)
+	checkErr(err)
+
+	//get url for current pfp
+	info, err := client.GetProfilePictureInfo(jid, &params)
+	checkErr(err)
+
+	println("\nOld PFP:", info.URL)
+
+	image, err := os.ReadFile("pfp.jpg")
+	if err != nil {
+		println("Picture should be a jpeg, and named pfp.jpg")
+		panic(err)
+	}
+
+	var answer string
+	fmt.Print("\nDISCLAIMER: The picture should be 720x720 or smaller.\nWould you like to continue (y/n): ")
+	_, err = fmt.Scanln(&answer)
+	checkErr(err)
+
+	if answer != "y" {
+		client.Disconnect()
+		os.Exit(0)
+	}
+
+	//update pfp
+	_, err = client.SetGroupPhoto(types.EmptyJID, image)
+	checkErr(err)
+
+	println("\nUpdated profile picture")
+
+	//get url for new pfp
+	info, err = client.GetProfilePictureInfo(jid, &params)
+	checkErr(err)
+
+	println("\nNew PFP:", info.URL)
+
+	//wait
+	println("\nDone!")
+	_, _ = fmt.Scanln(&answer)
+
+	client.Disconnect()
+	os.Exit(0)
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
